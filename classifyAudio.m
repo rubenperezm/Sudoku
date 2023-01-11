1328b8eafe9a9cb36cd10b94671becbea2fdd9e0
Paul <pablo.sanchi.herrera@gmail.com>
2023-01-11 18:10:01 +0100

    add comments to code classify

--- classifyAudio.m
+++ classifyAudio.m
@@ -4,33 +4,31 @@ function res = classifyAudio(signal)
 
     caracteristics = getC(signal);
     dataset = {};
-
+    
+    % cargar datos del dataset
     for i=1:10
         for j=1:10
             load(strcat('audioDataset/track', num2str(i-1), num2str(j-1)));
             subset(j,:) = signal;
         end
         dataset{i} = subset;
-    end
-
-%     db = cell2mat(dataset);
-%     save('audioDB', 'db');    
+    end   
     
+    % votacion
     W = zeros(1,10);
-    for i=1:width(dataset) % set of recordings
-        for j=1:10 % 0-9 of the i-subset
+    for i=1:width(dataset) % set de grabaciones
+        for j=1:10 % 0-9 del subconjunto i
             subset = dataset{i};
             signal2 = subset(j,:);
             caracteristics2 = getC(signal2);
-            Error(j) = dtw(caracteristics2, caracteristics);
+            Error(j) = dtw(caracteristics2, caracteristics); % obtencion de error
         end
-        [~,pos] = min(Error); % selected number
-        % remember that if the pos is 1,
-        % the number is 0, and not 1
-        W(pos) = W(pos) + 1;
+        [~,pos] = min(Error); % numero estimado
+        % 0-9 pero pos es de 1-10
+        W(pos) = W(pos) + 1; % añadir +1 al contador
     end
-    [~,pos] = max(W);
-    disp(['num: ', num2str(pos-1)]);
+    [~,pos] = max(W); % estimacion
+%     disp(['num: ', num2str(pos-1)]);
     
     res = pos-1;
 end
